cad0c2ddb10d683d6ce81e81e1afd6dbcfb48860
Kirill Tarasenko <ktarasenko003@gmail.com>
2024-04-14 23:20:46 +0300

    Validation improved

--- index.php
+++ index.php
@@ -2,13 +2,23 @@
 
 require_once 'vendor/autoload.php';
 
+function areAllElementsUnique(array $array): bool
+{
+    $uniqueArray = array_unique($array);
+    return count($uniqueArray) === count($array);
+}
+
 array_shift($argv);
-if (count($argv) >= 3) {
-    if (count($argv) % 2 !== 0) {
-        print_r($argv);
+if (areAllElementsUnique($argv)) {
+    if (count($argv) >= 3) {
+        if (count($argv) % 2 !== 0) {
+            print_r($argv);
+        } else {
+            echo 'You passed an even number of arguments. Please re-enter. Example: "rock Spock paper lizard scissors"';
+        }
     } else {
-        echo 'You passed an even number of arguments. Please re-enter. Example: "rock Spock paper lizard scissors"';
+        echo 'You passed the number of arguments less than 3. Please repeat your input. Example: "rock Spock paper lizard scissors"';
     }
 } else {
-    echo 'You passed the number of arguments less than 3. Please repeat your input. Example: "rock Spock paper lizard scissors"';
+    echo 'You passed arguments that included duplicate values. Please re-enter again. Example: “rock Spock paper lizard scissors.”';
 }
